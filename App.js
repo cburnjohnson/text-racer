e0b6b6e2c5cc718bb1c4bbbbf5ce8189ae22db1b
Cole Johnson <cburnjohnson@gmail.com>
2020-09-21 19:19:42 -0700

    added fonts

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const App = () => {
     return (
         <View>
-            <Text>Text Racer</Text>
+            <Text style={styles.header}>Text Racer</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 36,
+        textTransform: 'uppercase',
+        textAlign: 'center',
+        paddingTop: 10,
+        fontFamily: 'Roboto-Bold'
+    }
+});
 
 export default App;
