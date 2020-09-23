5a48d5498c801fdd10740a2d5b857d6e969dd958
Cole Johnson <cburnjohnson@gmail.com>
2020-09-22 20:12:00 -0700

    updates

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Home from './screens/Home';
+import Game from './screens/Game';
 
-const Drawer = createDrawerNavigator();
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 const Stack = createStackNavigator();
-const MaterialBottomTabs = createMaterialBottomTabNavigator();
-const MaterialTopTabs = createMaterialTopTabNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Drawer.Navigator>
-                <Drawer.Screen name="Home" />
-                <Drawer.Screen name="Game" />
-            </Drawer.Navigator>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 };
