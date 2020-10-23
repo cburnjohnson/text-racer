7a90db9abd2cbfb1598cbaa28db97fb2424ae515
Cole Johnson <cburnjohnson@gmail.com>
2020-10-22 19:47:42 -0700

    register user api call

--- client/App.js
+++ client/App.js
@@ -8,7 +8,7 @@ import HomeScreen from './screens/HomeScreen';
 import GameScreen from './screens/GameScreen';
 import ResultsScreen from './screens/ResultsScreen';
 import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen'
+import RegisterScreen from './screens/RegisterScreen';
 
 import GameState from './context/game/GameState';
 import AuthState from './context/auth/AuthState';
@@ -20,12 +20,21 @@ export default function App() {
         <AuthState>
             <GameState>
                 <NavigationContainer>
-                    <Stack.Navigator initialRouteName="Login">
+                    <Stack.Navigator initialRouteName="Register">
                         <Stack.Screen name="Login" component={LoginScreen} />
-                        <Stack.Screen name="Register" component={RegisterScreen} />
+                        <Stack.Screen
+                            name="Register"
+                            component={RegisterScreen}
+                        />
                         <Stack.Screen name="Home" component={HomeScreen} />
-                        <Stack.Screen name="Text Racer" component={GameScreen} />
-                        <Stack.Screen name="Results" component={ResultsScreen} />
+                        <Stack.Screen
+                            name="Text Racer"
+                            component={GameScreen}
+                        />
+                        <Stack.Screen
+                            name="Results"
+                            component={ResultsScreen}
+                        />
                     </Stack.Navigator>
                 </NavigationContainer>
             </GameState>
